138cbbf522b8ffb5e8556585e753acc64548216f
kuboryoya <kubo.ryoya822@gmail.com>
2024-03-02 02:24:59 +0900

    Revert "fix"
    This reverts commit 231d771fbf38be2883b3f64c25593409bbb67ed1.

--- index.js
+++ index.js
@@ -15,30 +15,30 @@ const express = require('express');
 
 const app = express();
 
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// メッセージ受信時の処理
+client.on('messageCreate', message => {
+  // メッセージがBot自身のものだった場合は無視する
+  console.log('------------message------------')
+  console.info(message);
+  if (message.author.bot) return;
+
+  // メッセージが「おはよう」という内容だった場合に返信する
+  if (message.content.toLowerCase() === 'おはよう') {
+      message.reply('おはようございます！');
+  }
+});
+client.login(TOKEN);
+
 app.get('/', async (req,res) =>{
-
-  // Create a new client instance
-  const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-  // When the client is ready, run this code (only once).
-  // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-  // It makes some properties non-nullable.
-  client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-  });
-
-  // メッセージ受信時の処理
-  client.on('messageCreate', message => {
-    // メッセージがBot自身のものだった場合は無視する
-    console.log('------------message------------')
-    console.info(message);
-    if (message.author.bot) return;
-
-    // メッセージが「おはよう」という内容だった場合に返信する
-    if (message.content.toLowerCase() === 'おはよう') {
-        message.reply('おはようございます！');
-    }
-  });
-
   // Log in to Discord with your client's token
   client.login(TOKEN);
   return res.send('Follow documentation ')
